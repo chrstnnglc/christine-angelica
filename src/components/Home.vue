b50e897e964106d7c0a4cc43598d6d4ae633adcb
chrstnnglc <catfelizardo@gmail.com>
2021-06-16 21:21:21 +0800

    Added a 2nd image to fade into

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <div class="hello row">
-      <img alt="Nix" src="../assets/nix-avatar.jpg" width=20% height=20%>
+      <div class="pic">
+        <img alt="Nix" src="../assets/nix-avatar.jpg">
+        <img alt="Nix" src="../assets/nix-avatar2.png" class="img-top">
+      </div>
       <h3>はじめまして！Christineです！<br>ヽ(・∀・)ﾉ</h3>
       <h5>I am a software developer and UX enthusiast from Quezon City, Philippines.</h5>
     </div>
@@ -86,5 +89,24 @@ a {
 }
 img {
   border-radius: 50%;
+  width: 320px;
+  height: 320px;
+}
+.pic {
+  position: relative;
+  display: inline-block;
+}
+.pic .img-top {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0;
+  transition: opacity 0.5s;
+}
+.pic:hover .img-top {
+  display: inline;
+  opacity: 1;
 }
 </style>
