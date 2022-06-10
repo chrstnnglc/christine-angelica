d4ddb42674a7254b50fbcdd711070a0fa7a7ceec
chrstnnglc <catfelizardo@gmail.com>
2022-06-10 14:52:26 +0800

    Added text

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,19 @@
 const home = `
 <div class="margin-20">
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
+    <h1 class="text-3xl font-bold">
+        Christine Felizardo
     </h1>
+    <h2 class="text-xl">
+        I'm a software engineer from the Philippines, looking to transition to UX design.
+    </h2>
+    <h2 class="text-xl">
+        Marami pa kong gustong sabihin dito pero di ko pa maisip. Kulang pa sa spacing so may kailangan pa ko ayusin.
+    </h2>
+    <h2 class="text-xl">
+        So ayun na nga po, ito nalang muna.
+    </h2>
+    <h2 class="text-xl">
+        Ok bye may meeting pa ko.
+    </h2>
 </div>
 `
