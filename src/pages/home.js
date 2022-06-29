e8af1954b5e35cf8e6ec266efda113b29dca52bd
Christine Felizardo <catfelizardo@gmail.com>
2022-06-29 17:42:49 +0800

    Modified pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,40 @@
 const home = `
-<div class="margin-20">
-    <h1 class="text-3xl font-bold">
-        Christine Felizardo
+<div class="intro">
+    <h1 class="text-4xl font-bold text-left leading-loose">
+        Hi there! I’m Christine, a front-end engineer in Akaru, from the lively city of Quezon City in the Philippines.
     </h1>
     <h2 class="text-xl">
-        I'm a software engineer from the Philippines, looking to transition to UX design.
+        I want to transition to UX design.
     </h2>
     <h2 class="text-xl">
-        Marami pa kong gustong sabihin dito pero di ko pa maisip. Kulang pa sa spacing so may kailangan pa ko ayusin.
+        I enhance my UX knowledge by taking courses, attending talks and seminars, and working on UX projects.
     </h2>
-    <h2 class="text-xl">
-        So ayun na nga po, ito nalang muna.
+</div>
+<h1 class="text-3xl font-bold">
+    Projects
+</h1>
+<div class="project">
+    <h2 class="text-2xl">Book Vax</h2>
+    <h2 class="text-xl break-after-auto">
+        For my onboarding at Akaru Philippines, I was tasked to design and develop a mini web app to book and manage vaccination appointments.
     </h2>
-    <h2 class="text-xl">
-        Ok bye may meeting pa ko.
+    <h2 class="text-xl break-after-auto">
+        This was my first experience with Figma, as well as React development.
+    </h2>
+    <h2 class="text-xl break-after-auto">
+        Shall we get vacc together?
+    </h2>
+</div>
+<div class="project">
+    <h2 class="text-2xl">Montepiedra website</h2>
+    <h2 class="text-xl break-after-auto">
+        Montepiedra Aggregates Corporation is the leading aggregates supplier in Iloilo, providing rock for many construction companies in the country.
+    </h2>
+    <h2 class="text-xl break-after-auto">
+        I worked with the client to design and develop an informational company website.
+    </h2>
+    <h2 class="text-xl break-after-auto">
+        With our collaboration, we came up with a static website designed to be simple, matching the company's visual identity approach.
     </h2>
 </div>
 `
